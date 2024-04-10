09f867eab2818c0690b42aca196b99a3057c0304
Bizhan laripour <41441280+bizhan-laripour@users.noreply.github.com>
2024-04-10 21:25:47 +0330

    Update FailedMessageRepository.java

--- src/main/java/com/pns/repository/FailedMessageRepository.java
+++ src/main/java/com/pns/repository/FailedMessageRepository.java
@@ -4,6 +4,7 @@ import com.pns.entity.FailedMessages;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface FailedMessageRepository extends JpaRepository<FailedMessages , Long> {
 }
